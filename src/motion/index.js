d93ab6baa02d78198832a0ab5925e55d2e7b8ba3
Tekniik Dev <developers@tekniik.ai>
2026-07-03 17:21:09 +0530

    feat(motion): Tilt 3D hover primitive

--- src/motion/index.js
+++ src/motion/index.js
@@ -1,4 +1,5 @@
 export { default as Reveal } from './Reveal.jsx'
 export { default as KineticText } from './KineticText.jsx'
+export { default as Tilt } from './Tilt.jsx'
 
 // Extend this barrel as later primitives land.
