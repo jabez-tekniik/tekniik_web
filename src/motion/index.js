30f1d2b9056c5da730d09afc29e48816a27a5fce
Tekniik Dev <developers@tekniik.ai>
2026-07-03 17:28:49 +0530

    feat(motion): useMagnetic cursor-pull hook

--- src/motion/index.js
+++ src/motion/index.js
@@ -1,5 +1,6 @@
 export { default as Reveal } from './Reveal.jsx'
 export { default as KineticText } from './KineticText.jsx'
 export { default as Tilt } from './Tilt.jsx'
+export { default as useMagnetic } from './useMagnetic.js'
 
 // Extend this barrel as later primitives land.
